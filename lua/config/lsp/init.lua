5f7b96690ac51bfef23b3a18531acb1d14e262f4
MLTR <mikailoudem@gmail.com>
2025-12-06 22:38:57 +0100

    deleted all cpp lsp stuff
    I felt like having a clean start would help

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -7,11 +7,3 @@ lua_ls.capabilities = functions.capabilities
 vim.lsp.config.lua_ls = lua_ls
 vim.lsp.enable("lua_ls")
 
--- clangd
-local clangd = require("config.lsp.clangd")
-
-clangd.capabilities = clangd.capabilities
-
-local old_on_attach = clangd.on_attach
-
-vim.lsp.config.clangd = clangd
